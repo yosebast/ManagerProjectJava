43360823c6132e31b3229ef9fec7a11117693bea
yosebast <ik_sbtian@yahoo.es>
2018-02-05 21:53:23 +0100

    subida de prueba

---
+++ src/main/java/com/melo/manager/today/CalculaFecha.java
@@ -0,0 +1,16 @@
+package com.melo.manager.today;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalculaFecha {
+	
+	public Date obtieneFecha(){
+		Date fecha =  Calendar.getInstance().getTime();
+		
+		return fecha;
+	}
+	
+	
+
+}
